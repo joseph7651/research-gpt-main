a62f5ee8b70d60e94039fc33651e224bf526110e
joseph7651 <josephcastellano715@gmail.com>
2025-06-09 00:00:00 +0100

    chore: update models

--- dashboard/models.py
+++ dashboard/models.py
@@ -61,3 +61,4 @@ from django.db import models
 # touched on 2025-08-14T20:13:06.180443Z
 # touched on 2025-08-14T20:13:10.396591Z
 # touched on 2025-08-14T20:13:16.711738Z
+# touched on 2025-08-14T20:13:31.166081Z
